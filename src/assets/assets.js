c147f148e71539228ae98854d9b0aa4c0b099950
Mq <voronin.fedor.99@gmail.com>
2025-09-15 09:08:27 +0300

    Add files via upload

---
+++ src/assets/assets.js
@@ -0,0 +1,51 @@
+import basket from "./basket.svg";
+import headerImage from "./headerImage.png";
+import byRoom1 from "./byRoom1.png";
+import byRoom2 from "./byRoom2.png";
+import byRoom3 from "./byRoom3.png";
+import byRoom4 from "./byRoom4.png";
+import door from "./door.svg";
+import garage from "./garageSale.png";
+import best1 from "./bestsellers1.png";
+import best2 from "./bestsellers2.png";
+import best3 from "./bestsellers3.png";
+import best4 from "./bestsellers4.png";
+import brand1 from "./brand1.svg";
+import brand2 from "./brand2.svg";
+import brand3 from "./brand3.svg";
+import brand4 from "./brand4.svg";
+import brand5 from "./brand5.svg";
+import facebook from "./facebook.svg";
+import twitter from "./twitter.svg";
+import pint from "./pint.svg";
+import logo from "./logo.svg";
+import linkIN from "./linkIN.svg";
+import inst from "./inst.svg";
+import girl from "./girl.svg";
+import furniture2 from "./furniture2.png";
+import furniture from "./furnitur.png";
+import floyd from "./floyd.png";
+import enjoy from "./enjoy.png";
+import floydSvg from "./floyd.svg";
+import arrow from "./arrow.svg";
+
+const assets = {
+  arrow,
+  floydSvg,
+  enjoy,
+  girl,
+  furniture2,
+  furniture,
+  floyd,
+  logo,
+  door,
+  basket,
+  headerImage,
+  garage,
+  social: { facebook, inst, twitter, linkIN, pint },
+  byRoom: { byRoom1, byRoom2, byRoom3, byRoom4 },
+  bestsellers: { best1, best2, best3, best4 },
+  brands: { brand1, brand2, brand3, brand4, brand5 },
+};
+
+export default assets;
